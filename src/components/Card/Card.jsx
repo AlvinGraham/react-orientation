88d923ddf668b68504672bbf7b4e0af31c98fb49
Alvin Graham <grah0071@gmail.com>
2024-01-09 21:06:05 -0600

    Add count button functionality

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,17 +4,24 @@ import { useState } from 'react';
 
 function Card(props) {
   const [myName, setMyName] = useState('Alvin');
-
+  const [count, setCount] = useState(0);
+  
+  
   function clickChangeName() {
     // set name to new name
     console.log('Test');
     setMyName('Alvin Graham');
   }
 
+  function countButtonClick() {
+    setCount(count + 1);
+  }
+
   return(
     <div className="card">
-    <button>
-      count is 0
+    {/* create state to increment the count */}
+    <button onClick={countButtonClick}>
+      count is {count}
     </button>
     <p>{props.globalName ? props.globalName : myName}</p>
     <button onClick={clickChangeName}>Change Name</button>
